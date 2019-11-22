79f003d977cbc4f138544437a18384ef8cb8dfa5
Marcin Bajek <bajekmarcin@gmail.com>
2019-11-22 11:48:41 +0100

    Add cards

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,18 +1,42 @@
 import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
+import Card from '../Card/Card.js';
+import Creator from '../Creator/Creator.js'
 
 
 class Column extends React.Component {
+  state = {
+    cards: this.props.cards || [],
+  }
   static propTypes = {
-    columnName: PropTypes.node.isRequired,
+    title: PropTypes.node.isRequired,
+    cards: PropTypes.array,
     }
   render() {
     return (
       <section className={styles.component}>
-        <h3 className={styles.title}>{this.props.columnName}</h3>
+        <h3 className={styles.title}>{this.props.title}</h3>
+        {this.state.cards.map(({key, ...cardProps}) => (
+          <Card key={key} {...cardProps} />
+        ))}
+        
       </section>
     )
+  }
+
+  addCard(title){
+    this.setState(state => (
+      {
+        cards: [
+          ...state.cards,
+          {
+            key: state.cards.length ? state.cards[state.cards.length-1].key+1 : 0,
+            title
+          }
+        ]
+      }
+    ));
   }
 }
 
