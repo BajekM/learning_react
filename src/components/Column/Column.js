6d60a48a9bb57abd25cbfe42d8fedef1aac7fc7b
Marcin Bajek <bajekmarcin@gmail.com>
2019-11-22 12:39:35 +0100

    Finish app

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -2,7 +2,8 @@ import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
 import Card from '../Card/Card.js';
-import Creator from '../Creator/Creator.js'
+import Creator from '../Creator/Creator.js';
+import {settings} from '../../data/dataStore';
 
 
 class Column extends React.Component {
@@ -20,11 +21,12 @@ class Column extends React.Component {
         {this.state.cards.map(({key, ...cardProps}) => (
           <Card key={key} {...cardProps} />
         ))}
-        
+        <div className={styles.creator}>
+          <Creator text={settings.cardCreatorText} action={title => this.addCard(title)}/>
+        </div>
       </section>
     )
   }
-
   addCard(title){
     this.setState(state => (
       {
@@ -32,7 +34,9 @@ class Column extends React.Component {
           ...state.cards,
           {
             key: state.cards.length ? state.cards[state.cards.length-1].key+1 : 0,
-            title
+            title,
+            icon: 'list-alt',
+            cards: []
           }
         ]
       }
