3394f85698390dbe5c541afb0f875a9655829c39
Marcin Bajek <bajekmarcin@gmail.com>
2019-11-28 03:15:39 +0100

    Repair mistakes

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -13,7 +13,7 @@ class Column extends React.Component {
   static propTypes = {
     title: PropTypes.node.isRequired,
     cards: PropTypes.array,
-    }
+  }
   render() {
     return (
       <section className={styles.component}>
@@ -25,7 +25,7 @@ class Column extends React.Component {
           <Creator text={settings.cardCreatorText} action={title => this.addCard(title)}/>
         </div>
       </section>
-    )
+    );
   }
   addCard(title){
     this.setState(state => (
@@ -36,9 +36,9 @@ class Column extends React.Component {
             key: state.cards.length ? state.cards[state.cards.length-1].key+1 : 0,
             title,
             icon: 'list-alt',
-            cards: []
-          }
-        ]
+            cards: [],
+          },
+        ],
       }
     ));
   }
