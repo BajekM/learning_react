0d62f1c2bf9b497acbd303983ef66f0468b41373
Marcin Bajek <bajekmarcin@gmail.com>
2019-12-02 01:24:48 +0100

    Add Container

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -3,7 +3,8 @@ import styles from './Search.scss';
 import Button from '../Button/Button.js';
 import PropTypes from 'prop-types';
 import { settings } from '../../data/dataStore.js';
-import Icon from '../Icon/Icon';
+import Icon from '../Icon/Icon.js';
+import Container from '../Container/Container.js';
 
 class Search extends React.Component {
   static propTypes = {
@@ -45,19 +46,22 @@ class Search extends React.Component {
     const {icon} = settings.search;
     return (
       <div className={styles.component}>
-        <input
-          type='text'
-          placeholder={text}
-          value={value}
-          onChange={event => this.handleChange(event)}
-        />
-        <div className={styles.buttons}>
-          <Button onClick={() => this.handleOK()}><Icon name={icon} /></Button>
-        </div>
-        <div>
-          { countVisible == countAll ? '' : `${countVisible} / ${countAll}` }
-        </div>
+        <Container>
+          <input
+            type='text'
+            placeholder={text}
+            value={value}
+            onChange={event => this.handleChange(event)}
+          />
+          <div className={styles.buttons}>
+            <Button onClick={() => this.handleOK()}><Icon name={icon} /></Button>
+          </div>
+          <div>
+            { countVisible == countAll ? '' : `${countVisible} / ${countAll}` }
+          </div>
+        </Container>
       </div>
+      
     );
   }
 }
