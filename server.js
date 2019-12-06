6edc6a7edc2f21cb55e9b0404b100651325ca877
Marcin Bajek <bajekmarcin@gmail.com>
2019-12-06 05:58:37 +0100

    Change server.js

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 
-const httpServer = require('http-server');
-const server = httpServer.create();
+import { create, defaults } from 'http-server';
+const server = create();
 // const router = httpServer.router('dist/db/app.json');
-const middlewares = httpServer.defaults({
+const middlewares = defaults({
   static: 'dist',
   noCors: true,
 });
-const port = process.env.PORT || 3131;
+const port = process.env.PORT || 8080;
 
 server.use(middlewares);
 // server.use(router);
